10491b4d85c4dca94f7b29f322306a0f856355b9
AcemThe0 <52658045+AcemThe0@users.noreply.github.com>
2022-10-08 16:35:48 +0300

    Added a smaller cap to timer

--- src/main/java/acme/jungleware/jungle/module/fight/timer.java
+++ src/main/java/acme/jungleware/jungle/module/fight/timer.java
@@ -3,18 +3,21 @@ package acme.jungleware.jungle.module.fight;
 import acme.jungleware.jungle.module.Mod;
 import acme.jungleware.jungle.module.settings.NumberSetting;
 import acme.jungleware.jungle.ui.screens.clickgui.setting.Slider;
+import acme.jungleware.jungle.module.settings.BooleanSetting;
+import acme.jungleware.jungle.ui.screens.clickgui.setting.CheckBox;
 
 public class timer extends Mod {
-    public static NumberSetting multiplier = new NumberSetting("Multiplier", 0.10, 20.00, 1.00, 0.10);
+    public static NumberSetting multiplier = new NumberSetting("Multiplier", 0.10, 5.00, 1.00, 0.10);
+    public static BooleanSetting enabled = new BooleanSetting("Enabled", false);
+
 
     public timer() {
         super("Timer", "Limitless.", Category.CHIMP);
-        addSetting(multiplier);
+        addSettings(multiplier, enabled);
     }
 
     public static double getMultiplier() {
-        return multiplier.getValueFloat();
-
+        return enabled.isEnabled() ? multiplier.getValueFloat() : 1;
     }
 
 }
